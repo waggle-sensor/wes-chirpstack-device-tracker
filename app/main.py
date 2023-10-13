f8c113547c0108189d37aa3bd60002393b9b0b07
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-10-13 15:56:54 -0500

    implemented chirpstackclient class

--- app/main.py
+++ app/main.py
@@ -4,43 +4,19 @@ import datetime
 import pytz 
 import grpc
 from chirpstack_api import api
+from client import ChirpstackClient
 
+#TODO: implement the get apps api to get all devices
+APP_ID = "ac81e18b-1925-47f9-839a-27d999a8af55"
 
-
-# Configuration.
-server = "http://wes-chirpstack-server:8080"
-
-# The DevEUI for which you want to enqueue the downlink.
-#dev_eui = "0101010101010101"
-
-# The API token (retrieved using the web-interface).
-W030_api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjaGlycHN0YWNrIiwiaXNzIjoiY2hpcnBzdGFjayIsInN1YiI6IjE0ZmQxYjU4LTU5MGItNDE2MS1iNjc2LTdlNzY3YTc1MGY0ZiIsInR5cCI6ImtleSJ9.Xhwz-tblD-zWXTzPnHIqutk7I5xazaddT2eGBzKALt4"
-W039_api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjaGlycHN0YWNrIiwiaXNzIjoiY2hpcnBzdGFjayIsInN1YiI6IjllZGVjNmY5LTIwYzAtNGMyYy04ZDdiLTBkOWExN2M2YTE5NiIsInR5cCI6ImtleSJ9.1F_mN6Q7rdJI2e_qeeUAaSychBVsoCE2uS51INfbktk"
-api_token = W039_api_token
+#TODO: uncomment the wes-chirpstack-sever when implemented in kubernetes cluster
+#server = "http://wes-chirpstack-server:8080"
+server = "localhost:8080"
 
 if __name__ == "__main__":
-    # Connect without using TLS.
-    channel = grpc.insecure_channel(server)
+    chirpstack_client = ChirpstackClient(server)
 
-    # Device-queue API client.
-    client = api.DeviceServiceStub(channel)
-
-    # Define the API key meta-data.
-    auth_token = [("authorization", "Bearer %s" % api_token)]
-
-    # Construct request.
-    req = api.ListDevicesRequest()
-    req.limit = 5 #Max number of devices to return in the result-set.
-    #req.offset = 5 # Offset in the result-set (for pagination). Combination with limit only returns total_count
-    #req.search = "MFR Node" #If set, the given string will be used to search on name (optional).
-    req.application_id = "ac81e18b-1925-47f9-839a-27d999a8af55" #Application ID (UUID) to filter devices on.
-    #req.multicast_group_id = "" #Multicst-group ID (UUID) to filter devices on.
-    #req.queue_item.confirmed = False
-    #req.queue_item.data = bytes([0x01, 0x02, 0x03])
-    #req.queue_item.dev_eui = dev_eui
-    #req.queue_item.f_port = 10
-
-    resp = client.List(req, metadata=auth_token)
+    resp = chirpstack_client.list_devices(APP_ID)
 
     for device in resp.result:
         # Calculate the total seconds with nanoseconds
