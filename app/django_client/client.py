5cf18b0151df29dae7b03b76f356f85153ad8ec2
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-12-20 13:00:55 -0600

    added HttpMethod class to simplify call_api method

--- app/django_client/client.py
+++ app/django_client/client.py
@@ -4,6 +4,11 @@ import sys
 import os
 from urllib.parse import urljoin
 
+class HttpMethod(Enum):
+    GET = requests.get
+    POST = requests.post
+    PATCH = requests.patch
+
 class DjangoClient:
     """
     Django client to call Api(s)
@@ -25,100 +30,94 @@ class DjangoClient:
         Get LoRaWAN connection using dev EUI
         """
         api_endpoint = f"{self.LC_ROUTER}{self.vsn}/{dev_eui}/"
-        return  self.call_api("get", api_endpoint, data)
+        return  self.call_api(HttpMethod.GET, api_endpoint, data)
 
     def create_lc(self, data):
         """
         Create LoRaWAN connection
         """
         api_endpoint = f"{self.LC_ROUTER}"
-        return  self.call_api("post", api_endpoint, data)
+        return  self.call_api(HttpMethod.POST, api_endpoint, data)
 
     def update_lc(self, dev_eui, data):
         """
         Update LoRaWAN connection
         """
         api_endpoint = f"{self.LC_ROUTER}{self.vsn}/{dev_eui}/"
-        return  self.call_api("patch", api_endpoint, data)
+        return  self.call_api(HttpMethod.PATCH, api_endpoint, data)
 
     def get_ld(self, dev_eui):
         """
         Get LoRaWAN device using dev EUI
         """
         api_endpoint = f"{self.LD_ROUTER}{dev_eui}/"
-        return  self.call_api("get", api_endpoint, data)
+        return  self.call_api(HttpMethod.GET, api_endpoint, data)
 
     def create_ld(self, data):
         """
         Create LoRaWAN device
         """
         api_endpoint = f"{self.LD_ROUTER}"
-        return  self.call_api("post", api_endpoint, data)
+        return  self.call_api(HttpMethod.POST, api_endpoint, data)
 
     def update_ld(self, dev_eui, data):
         """
         Update LoRaWAN device
         """
         api_endpoint = f"{self.LD_ROUTER}{dev_eui}/"
-        return  self.call_api("patch", api_endpoint, data)
+        return  self.call_api(HttpMethod.PATCH, api_endpoint, data)
 
     def get_lk(self, dev_eui):
         """
         Get LoRaWAN key using dev EUI
         """
         api_endpoint = f"{self.LK_ROUTER}{self.vsn}/{dev_eui}/"
-        return  self.call_api("get", api_endpoint)
+        return  self.call_api(HttpMethod.GET, api_endpoint)
 
     def create_lk(self, data):
         """
         Create LoRaWAN key
         """
         api_endpoint = f"{self.LK_ROUTER}"
-        return  self.call_api("post", api_endpoint, data)
+        return  self.call_api(HttpMethod.POST, api_endpoint, data)
 
     def update_lk(self, dev_eui, data):
         """
         Update LoRaWAN key
         """
         api_endpoint = f"{self.LK_ROUTER}{self.vsn}/{dev_eui}/"
-        return  self.call_api("patch", api_endpoint, data)
+        return  self.call_api(HttpMethod.PATCH, api_endpoint, data)
 
     def get_sh(self, hw_model):
         """
         Get Sensor Hardware using hw_model
         """
         api_endpoint = f"{self.SH_ROUTER}{hw_model}/"
-        return  self.call_api("get", api_endpoint)
+        return  self.call_api(HttpMethod.GET, api_endpoint)
 
     def create_sh(self, data):
         """
         Create Sensor Hardware
         """
         api_endpoint = f"{self.SH_ROUTER}"
-        return self.call_api("post", api_endpoint, data)
+        return self.call_api(HttpMethod.POST, api_endpoint, data)
 
     def update_sh(self, hw_model, data):
         """
         Update Sensor Hardware 
         """
         api_endpoint = f"{self.SH_ROUTER}{hw_model}/"
-        return self.call_api("patch", api_endpoint, data)
+        return self.call_api(HttpMethod.PATCH, api_endpoint, data)
 
-    def call_api(self, action, endpoint, data):
+    def call_api(self, method: HttpMethod, endpoint: str, data: dict) -> dict:
         """
-        Create request based on the action and call the api
+        Create request based on the method and call the api
         """
-        api_url = urljoin(self.server, api_endpoint)
+        api_url = urljoin(self.server, endpoint)
 
-        actions_mapping = {
-            'get': requests.get,
-            'post': requests.post,
-            'patch': requests.patch,
-        }
+        if method not in HttpMethod:
+            raise ValueError(f"Unsupported method: {method}")
 
-        if action.lower() not in actions_mapping:
-            raise ValueError(f"Unsupported action: {action}")
-
-        response = actions_mapping[action.lower()](api_url, headers=headers, json=data)
+        response = method.value(api_url, headers=self.auth_header, json=data)
         response.raise_for_status() # Raise an exception for bad responses (4xx or 5xx)
         return response.json()
