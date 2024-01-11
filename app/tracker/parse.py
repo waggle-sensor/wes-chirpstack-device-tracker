2f106f3364af0b62585eab3cd8379b533119597c
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-11 10:24:43 -0600

    added new clean_hw_model f(x)

--- app/tracker/parse.py
+++ app/tracker/parse.py
@@ -1,8 +1,19 @@
-
+import re
 
 def replace_spaces(str: str) -> str:
     """
     replace spaces with "-"
     """
-    result = str.replace(" ", "-")
-    return result
+    temp = str.replace(" ", "-")
+    temp = re.sub(r'-+', '-', temp) # replace consecutive hyphens with a single hyphen
+    return temp
+
+def clean_hw_model(str: str) -> str:
+    """
+    Makes the string capatible with hw_model field
+    """
+    temp = re.sub(r'\([^)]*\)', '', str) # Remove characters inside parentheses including parantheses
+    temp = temp.strip()
+    temp = replace_spaces(temp)
+    temp = temp[:30]
+    return temp
