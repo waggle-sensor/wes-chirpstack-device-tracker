293ec560a3bd38451c79a655bee8cbe7bc937c21
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-12 17:10:42 -0600

    added test for Manifest.load_manifest()

--- test/test_manifest_class.py
+++ test/test_manifest_class.py
@@ -1,3 +1,39 @@
 import unittest
-from unittest.mock import Mock, patch, MagicMock
+from unittest.mock import Mock, patch, MagicMock, mock_open
 from app.manifest import Manifest
+
+MANIFEST_FILEPATH = '/etc/waggle/node-manifest-v2.json'
+
+class TestlLoadManifest(unittest.TestCase):
+    @patch('builtins.open', mock_open(read_data='{"key": "value"}'))
+    def test_load_manifest_existing_file(self):
+        """
+        Tests the case when the file exists and json.load successfully loads the content.
+        """
+        # Arrange
+        filepath = MANIFEST_FILEPATH
+        manifest = Manifest(filepath)
+
+        # Call the method under test
+        result = manifest.load_manifest()
+
+        # Assert
+        self.assertEqual(result, {"key": "value"})
+
+    @patch('builtins.open', side_effect=FileNotFoundError)
+    def test_load_manifest_missing_file(self, mock_open_file):
+        """
+        Tests the case when the file is not found, and the method returns an empty dictionary.
+        """
+        # Arrange
+        filepath = 'nonexistent_manifest'
+        manifest = Manifest(filepath)
+
+        # Call the method under test
+        result = manifest.load_manifest()
+
+        # Assert
+        self.assertEqual(result, {})
+
+if __name__ == '__main__':
+    unittest.main()
