8d9729ad3158c3c23753d45ac608925339235633
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-15 14:23:28 -0600

    added tests for has_requiredKeys()

--- test/test_manifest_class.py
+++ test/test_manifest_class.py
@@ -1,6 +1,6 @@
 import unittest
-from app.manifest import Manifest
 import json
+from app.manifest import Manifest
 from unittest.mock import (
     Mock, 
     patch, 
@@ -429,6 +429,193 @@ class TestIsValidStruc(unittest.TestCase):
         }
 
         self.assertFalse(self.manifest.is_valid_struc(data))
+
+class TestHasRequiredKeys(unittest.TestCase):
+    def setUp(self):
+        self.filepath = MANIFEST_FILEPATH
+        self.manifest = Manifest(self.filepath)
+
+    def test_data_has_keys(self):
+        """
+        Test data has required keys
+        """
+        # Arrange
+        data = {
+            "connection_name": "SFM",
+            "created_at": "2023-12-13T19:47:45.355000Z",
+            "last_seen_at": "2023-12-13T19:47:45.355000Z",
+            "margin": 5,
+            "expected_uplink_interval_sec": 40,
+            "connection_type": "OTAA",
+            "lorawandevice": {
+                "deveui": "123456",
+                "name": "SFM1x Sap Flow",
+                "battery_level": 10,
+                "hardware": {
+                    "hardware": "Sap Flow Meter",
+                    "hw_model": "SFM1x",
+                    "hw_version": "",
+                    "sw_version": "",
+                    "manufacturer": "ICT International",
+                    "datasheet": "https://ictinternational.com/manuals-and-brochures/sfm1x-sap-flow-meter/",
+                    "capabilities": [
+                        "lorawan"
+                    ],
+                    "description": "# SFM1x Sap Flow Meter\r\n\r\nThe SFM1x Sap Flow Meter enables individual tree water use and health to be monitored in real time. This is because the SFM has integrated data transmission direct to cloud using IoT/LTE-M Cat-M1. The SFM1x Sap Flow Meter is a discrete standalone instrument based upon the Heat Ratio Method. This measurement principle has proven to be a robust and flexible technique to measure plant water use; being able to measure high, low, zero and reverse flows in a large range of plant anatomies & species from herbaceous to woody, and stem sizes > 10 mm in diameter. The theoretical basis and ratio metric design of the Heat Ratio Method makes possible the measurement of high, low, zero and reverse flows. The SFM1x Sap Flow Meter consists of two temperature sensing needles arranged equidistance above and below a central heater. These needles are inserted into the water conducting tissue of the plant by drilling 3 small parallel holes. Heat is then pulsed every 10 minutes into the water conducting tissue of the plant. The heat is used as a tracer to directly measure the velocity of water movement in the plant stem."
+                }
+            }
+        }
+
+        self.assertTrue(self.manifest.has_requiredKeys(data))
+
+    def test_lorawan_connection_not_have_keys(self):
+        """
+        Test lorawan connection does not have required keys
+        """
+        # Arrange
+        data = {
+            "connection_name": "SFM",
+            "created_at": "2023-12-13T19:47:45.355000Z",
+            "last_seen_at": "2023-12-13T19:47:45.355000Z",
+            "margin": 5,
+            "expected_uplink_interval_sec": 40,
+            "lorawandevice": {
+                "deveui": "123456",
+                "name": "SFM1x Sap Flow",
+                "battery_level": 10,
+                "hardware": {
+                    "hardware": "Sap Flow Meter",
+                    "hw_model": "SFM1x",
+                    "hw_version": "",
+                    "sw_version": "",
+                    "manufacturer": "ICT International",
+                    "datasheet": "https://ictinternational.com/manuals-and-brochures/sfm1x-sap-flow-meter/",
+                    "capabilities": [
+                        "lorawan"
+                    ],
+                    "description": "# SFM1x Sap Flow Meter\r\n\r\nThe SFM1x Sap Flow Meter enables individual tree water use and health to be monitored in real time. This is because the SFM has integrated data transmission direct to cloud using IoT/LTE-M Cat-M1. The SFM1x Sap Flow Meter is a discrete standalone instrument based upon the Heat Ratio Method. This measurement principle has proven to be a robust and flexible technique to measure plant water use; being able to measure high, low, zero and reverse flows in a large range of plant anatomies & species from herbaceous to woody, and stem sizes > 10 mm in diameter. The theoretical basis and ratio metric design of the Heat Ratio Method makes possible the measurement of high, low, zero and reverse flows. The SFM1x Sap Flow Meter consists of two temperature sensing needles arranged equidistance above and below a central heater. These needles are inserted into the water conducting tissue of the plant by drilling 3 small parallel holes. Heat is then pulsed every 10 minutes into the water conducting tissue of the plant. The heat is used as a tracer to directly measure the velocity of water movement in the plant stem."
+                }
+            }
+        }
+
+        self.assertFalse(self.manifest.has_requiredKeys(data))
+
+    def test_lorawan_device_not_have_keys(self):
+        """
+        Test lorawan device does not have required keys
+        """
+        # Arrange
+        data = {
+            "connection_name": "SFM",
+            "created_at": "2023-12-13T19:47:45.355000Z",
+            "last_seen_at": "2023-12-13T19:47:45.355000Z",
+            "margin": 5,
+            "expected_uplink_interval_sec": 40,
+            "connection_type": "OTAA",
+            "lorawandevice": {
+                "name": "SFM1x Sap Flow",
+                "battery_level": 10,
+                "hardware": {
+                    "hardware": "Sap Flow Meter",
+                    "hw_model": "SFM1x",
+                    "hw_version": "",
+                    "sw_version": "",
+                    "manufacturer": "ICT International",
+                    "datasheet": "https://ictinternational.com/manuals-and-brochures/sfm1x-sap-flow-meter/",
+                    "capabilities": [
+                        "lorawan"
+                    ],
+                    "description": "# SFM1x Sap Flow Meter\r\n\r\nThe SFM1x Sap Flow Meter enables individual tree water use and health to be monitored in real time. This is because the SFM has integrated data transmission direct to cloud using IoT/LTE-M Cat-M1. The SFM1x Sap Flow Meter is a discrete standalone instrument based upon the Heat Ratio Method. This measurement principle has proven to be a robust and flexible technique to measure plant water use; being able to measure high, low, zero and reverse flows in a large range of plant anatomies & species from herbaceous to woody, and stem sizes > 10 mm in diameter. The theoretical basis and ratio metric design of the Heat Ratio Method makes possible the measurement of high, low, zero and reverse flows. The SFM1x Sap Flow Meter consists of two temperature sensing needles arranged equidistance above and below a central heater. These needles are inserted into the water conducting tissue of the plant by drilling 3 small parallel holes. Heat is then pulsed every 10 minutes into the water conducting tissue of the plant. The heat is used as a tracer to directly measure the velocity of water movement in the plant stem."
+                }
+            }
+        }
+
+        self.assertFalse(self.manifest.has_requiredKeys(data))
+
+    def test_hardware_not_have_keys(self):
+        """
+        Test hardware does not have required keys
+        """
+        # Arrange
+        data = {
+            "connection_name": "SFM",
+            "created_at": "2023-12-13T19:47:45.355000Z",
+            "last_seen_at": "2023-12-13T19:47:45.355000Z",
+            "margin": 5,
+            "expected_uplink_interval_sec": 40,
+            "connection_type": "OTAA",
+            "lorawandevice": {
+                "deveui": "123456",
+                "name": "SFM1x Sap Flow",
+                "battery_level": 10,
+                "hardware": {
+                    "hardware": "Sap Flow Meter",
+                    "hw_version": "",
+                    "sw_version": "",
+                    "manufacturer": "ICT International",
+                    "datasheet": "https://ictinternational.com/manuals-and-brochures/sfm1x-sap-flow-meter/",
+                    "capabilities": [
+                        "lorawan"
+                    ],
+                    "description": "# SFM1x Sap Flow Meter\r\n\r\nThe SFM1x Sap Flow Meter enables individual tree water use and health to be monitored in real time. This is because the SFM has integrated data transmission direct to cloud using IoT/LTE-M Cat-M1. The SFM1x Sap Flow Meter is a discrete standalone instrument based upon the Heat Ratio Method. This measurement principle has proven to be a robust and flexible technique to measure plant water use; being able to measure high, low, zero and reverse flows in a large range of plant anatomies & species from herbaceous to woody, and stem sizes > 10 mm in diameter. The theoretical basis and ratio metric design of the Heat Ratio Method makes possible the measurement of high, low, zero and reverse flows. The SFM1x Sap Flow Meter consists of two temperature sensing needles arranged equidistance above and below a central heater. These needles are inserted into the water conducting tissue of the plant by drilling 3 small parallel holes. Heat is then pulsed every 10 minutes into the water conducting tissue of the plant. The heat is used as a tracer to directly measure the velocity of water movement in the plant stem."
+                }
+            }
+        }
+
+        self.assertFalse(self.manifest.has_requiredKeys(data))
+
+    def test_has_required_keys_not_valid(self):
+        """
+        Test when data is not a valid json format
+        """
+        # Arrange
+        data = {
+            "connection_name": "SFM",
+            "created_at": "2023-12-13T19:47:45.355000Z",
+            "last_seen_at": "2023-12-13T19:47:45.355000Z",
+            "margin": 1 + 2j, #<- not valid
+            "expected_uplink_interval_sec": 40,
+            "connection_type": "OTAA",
+            "lorawandevice": {
+                "deveui": "123456",
+                "name": "SFM1x Sap Flow",
+                "battery_level": 10,
+                "hardware": {
+                    "hardware": "Sap Flow Meter",
+                    "hw_model": "SFM1x",
+                    "hw_version": "",
+                    "sw_version": "",
+                    "manufacturer": "ICT International",
+                    "datasheet": "https://ictinternational.com/manuals-and-brochures/sfm1x-sap-flow-meter/",
+                    "capabilities": [
+                        "lorawan"
+                    ],
+                    "description": "# SFM1x Sap Flow Meter\r\n\r\nThe SFM1x Sap Flow Meter enables individual tree water use and health to be monitored in real time. This is because the SFM has integrated data transmission direct to cloud using IoT/LTE-M Cat-M1. The SFM1x Sap Flow Meter is a discrete standalone instrument based upon the Heat Ratio Method. This measurement principle has proven to be a robust and flexible technique to measure plant water use; being able to measure high, low, zero and reverse flows in a large range of plant anatomies & species from herbaceous to woody, and stem sizes > 10 mm in diameter. The theoretical basis and ratio metric design of the Heat Ratio Method makes possible the measurement of high, low, zero and reverse flows. The SFM1x Sap Flow Meter consists of two temperature sensing needles arranged equidistance above and below a central heater. These needles are inserted into the water conducting tissue of the plant by drilling 3 small parallel holes. Heat is then pulsed every 10 minutes into the water conducting tissue of the plant. The heat is used as a tracer to directly measure the velocity of water movement in the plant stem."
+                }
+            }
+        }
+
+        self.assertFalse(self.manifest.has_requiredKeys(data))
+
+    def test_has_required_keys_type_error(self):
+        """
+        Test when data has a wrong type for a key-value pair
+        """
+        # Arrange
+        data = {
+            "connection_name": "SFM",
+            "created_at": "2023-12-13T19:47:45.355000Z",
+            "last_seen_at": "2023-12-13T19:47:45.355000Z",
+            "margin": 5,
+            "expected_uplink_interval_sec": 40,
+            "connection_type": "OTAA",
+            "lorawandevice": 0 # <- wrong type
+        }
+
+
+        self.assertFalse(self.manifest.has_requiredKeys(data))
+
+        
 
 
 if __name__ == '__main__':
