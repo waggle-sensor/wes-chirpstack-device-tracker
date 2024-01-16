6222f324fef860b327d0c09c62f16ee7a2c4cb73
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-16 14:23:45 -0600

    added test for create_lc()

--- test/test_tracker_class.py
+++ test/test_tracker_class.py
@@ -1,4 +1,5 @@
 import unittest
+import requests
 from unittest.mock import Mock, patch, MagicMock
 from app.chirpstack_client import ChirpstackClient
 from app.django_client import DjangoClient, HttpMethod
@@ -183,7 +184,7 @@ class TestCreateLd(unittest.TestCase):
         # Call the action in testing
         self.tracker.create_ld(mock_dev_eui, mock_sh_id, device_resp)
 
-        #update data that should have been used
+        #create data that should have been used
         data = {
             "name": replace_spaces(self.gd_ret_val.device.name),
             "battery_level": self.gd_ret_val.device_status.battery_level,
@@ -273,3 +274,155 @@ class TestUpdateLc(unittest.TestCase):
 
         # Assertions
         mock_django_patch.assert_called_once_with(f"{API_INTERFACE}/lorawanconnections/{VSN}/{mock_dev_eui}/", headers=self.tracker.d_client.auth_header, json=data)
+
+class TestCreateLc(unittest.TestCase):
+
+    @patch('app.chirpstack_client.grpc.insecure_channel')
+    def setUp(self, mock_insecure_channel):
+        self.args = Mock(
+            api_interface=API_INTERFACE,
+            lorawanconnection_router=LC_ROUTER,
+            lorawankey_router=LK_ROUTER,
+            lorawandevice_router=LD_ROUTER,
+            sensorhardware_router=SH_ROUTER,
+            vsn=VSN,
+            node_token=NODE_TOKEN,
+            chirpstack_api_interface=CHIRPSTACK_API_INTERFACE,
+            chirpstack_account_email=CHIRPSTACK_ACT_EMAIL,
+            chirpstack_account_password=CHIRPSTACK_ACT_PASSWORD
+        )
+        #set up tracker
+        self.tracker = Tracker(self.args)
+        #mock ChirpstackClient.get_device() return value
+        self.gd_ret_val = Mock_gd_ret_val() 
+        #mock ChirpstackClient.get_device_profile() return value
+        self.gdp_ret_val = Mock_gdp_ret_val()
+
+    @patch("app.django_client.HttpMethod.POST")
+    @patch('app.chirpstack_client.api.DeviceProfileServiceStub')
+    @patch('app.chirpstack_client.api.DeviceServiceStub')
+    @patch('app.chirpstack_client.grpc.insecure_channel')
+    def test_create_lc_happy_path(self, mock_insecure_channel, mock_device_service_stub, mock_device_profile_service_stub, mock_django_post):
+        """
+        Successfully use chirpstack lorawan device and device profile data 
+        to call DjangoClient.create_lc()
+        """
+        # Mock the gRPC channel
+        mock_channel = Mock()
+        mock_insecure_channel.return_value = mock_channel
+
+        # Mock the DeviceServiceStub
+        mock_device_service_stub_instance = mock_device_service_stub.return_value
+        mock_device_service_stub_instance.Get.return_value = self.gd_ret_val
+
+        # Mock the DeviceProfileServiceStub
+        mock_device_profile_service_stub_instance = mock_device_profile_service_stub.return_value
+        mock_device_profile_service_stub_instance.Get.return_value = self.gdp_ret_val
+
+        # Create a ChirpstackClient instance
+        chirpstack_client = ChirpstackClient(self.args)
+
+        # Mock the dev_eui
+        mock_dev_eui = "mock_dev_eui"
+
+        # Call chirpstack_client get_device
+        device_resp = chirpstack_client.get_device(mock_dev_eui)
+
+        # Mock the device profile ID
+        mock_device_profile_id = "mock_device_profile_id"
+
+        # Call get_device_profile
+        deviceprofile_resp = chirpstack_client.get_device_profile(mock_device_profile_id)
+
+        # Call the action in testing
+        lc_uid = self.tracker.create_lc(mock_dev_eui, device_resp, deviceprofile_resp)
+
+        #create data that should have been used
+        datetime_obj_utc = self.tracker.epoch_to_UTC(
+            self.gd_ret_val.last_seen_at.seconds, 
+            self.gd_ret_val.last_seen_at.nanos
+        )        
+        last_seen_at = datetime_obj_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+        con_type = "OTAA" if self.gdp_ret_val.device_profile.supports_otaa else "ABP"
+        data = {
+            "node": VSN,
+            "lorawan_device": mock_dev_eui,
+            "connection_name": replace_spaces(self.gd_ret_val.device.name),
+            'last_seen_at': last_seen_at, 
+            "margin": self.gd_ret_val.device_status.margin,
+            "expected_uplink_interval_sec": self.gdp_ret_val.device_profile.uplink_interval,
+            "connection_type": con_type
+        }
+
+        # Assertions
+        mock_django_post.assert_called_once_with(f"{API_INTERFACE}/lorawanconnections/", headers=self.tracker.d_client.auth_header, json=data)
+        self.assertEqual(lc_uid, VSN + "-" + replace_spaces(device_resp.device.name) + "-" + mock_dev_eui)
+
+    @patch("app.django_client.HttpMethod.POST")
+    @patch('app.chirpstack_client.api.DeviceProfileServiceStub')
+    @patch('app.chirpstack_client.api.DeviceServiceStub')
+    @patch('app.chirpstack_client.grpc.insecure_channel')
+    def test_create_lc_no_response(self, mock_insecure_channel, mock_device_service_stub, mock_device_profile_service_stub, mock_django_post):
+        """
+        Test when DjangoClient.create_lc() returns no response
+        """
+        #Mock POST response
+        mock_response = Mock()
+        mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError("HTTP Error")
+        mock_django_post.return_value = mock_response
+
+        # Mock the gRPC channel
+        mock_channel = Mock()
+        mock_insecure_channel.return_value = mock_channel
+
+        # Mock the DeviceServiceStub
+        mock_device_service_stub_instance = mock_device_service_stub.return_value
+        mock_device_service_stub_instance.Get.return_value = self.gd_ret_val
+
+        # Mock the DeviceProfileServiceStub
+        mock_device_profile_service_stub_instance = mock_device_profile_service_stub.return_value
+        mock_device_profile_service_stub_instance.Get.return_value = self.gdp_ret_val
+
+        # Create a ChirpstackClient instance
+        chirpstack_client = ChirpstackClient(self.args)
+
+        # Mock the dev_eui
+        mock_dev_eui = "mock_dev_eui"
+
+        # Call chirpstack_client get_device
+        device_resp = chirpstack_client.get_device(mock_dev_eui)
+
+        # Mock the device profile ID
+        mock_device_profile_id = "mock_device_profile_id"
+
+        # Call get_device_profile
+        deviceprofile_resp = chirpstack_client.get_device_profile(mock_device_profile_id)
+
+        #data that should have been used
+        datetime_obj_utc = self.tracker.epoch_to_UTC(
+            self.gd_ret_val.last_seen_at.seconds, 
+            self.gd_ret_val.last_seen_at.nanos
+        )        
+        last_seen_at = datetime_obj_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+        con_type = "OTAA" if self.gdp_ret_val.device_profile.supports_otaa else "ABP"
+        data = {
+            "node": VSN,
+            "lorawan_device": mock_dev_eui,
+            "connection_name": replace_spaces(self.gd_ret_val.device.name),
+            'last_seen_at': last_seen_at, 
+            "margin": self.gd_ret_val.device_status.margin,
+            "expected_uplink_interval_sec": self.gdp_ret_val.device_profile.uplink_interval,
+            "connection_type": con_type
+        }
+
+        # Assertions
+        with self.assertLogs(level='ERROR') as log:            
+            # Call the method under test
+            lc_uid = self.tracker.create_lc(mock_dev_eui, device_resp, deviceprofile_resp)
+
+            # Assert
+            self.assertEqual(len(log.output), 3)
+            self.assertEqual(len(log.records), 3)
+            self.assertIn("Tracker.create_lc(): d_client.create_lc() did not return a response", log.output[2])
+            mock_django_post.assert_called_once_with(f"{API_INTERFACE}/lorawanconnections/", headers=self.tracker.d_client.auth_header, json=data)
+            self.assertIsNone(lc_uid)
