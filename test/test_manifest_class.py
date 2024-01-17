048204b90c3ea88a5a6cb167a58f5a72f4fa899d
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-01-17 12:24:22 -0600

    added description since this is a part of manifest_sample

--- test/test_manifest_class.py
+++ test/test_manifest_class.py
@@ -680,7 +680,8 @@ class TestUpdateManifest(unittest.TestCase):
                     "datasheet": "https://ictinternational.com/manuals-and-brochures/sfm1x-sap-flow-meter/",
                     "capabilities": [
                         "lorawan"
-                    ]
+                    ],
+                    "description": "# SFM1x Sap Flow Meter\r\n\r\nThe SFM1x Sap Flow Meter enables individual tree water use and health to be monitored in real time. This is because the SFM has integrated data transmission direct to cloud using IoT/LTE-M Cat-M1. The SFM1x Sap Flow Meter is a discrete standalone instrument based upon the Heat Ratio Method. This measurement principle has proven to be a robust and flexible technique to measure plant water use; being able to measure high, low, zero and reverse flows in a large range of plant anatomies & species from herbaceous to woody, and stem sizes > 10 mm in diameter. The theoretical basis and ratio metric design of the Heat Ratio Method makes possible the measurement of high, low, zero and reverse flows. The SFM1x Sap Flow Meter consists of two temperature sensing needles arranged equidistance above and below a central heater. These needles are inserted into the water conducting tissue of the plant by drilling 3 small parallel holes. Heat is then pulsed every 10 minutes into the water conducting tissue of the plant. The heat is used as a tracer to directly measure the velocity of water movement in the plant stem."
                 }
             }
         }
